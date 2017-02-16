d728bebdd82cb3a899f4f363601fd44878cded15
MedoElshamy <medo.mazag@gmail.com>
2017-02-16 14:16:57 +0200

    C# Hello World
    This is the first project in C# including the hello world program

---
+++ FirstPrograme/FirstPrograme/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstPrograme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my first program in CSharp
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
